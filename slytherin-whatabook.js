202bf487ed6180e8d741b4e5ebfee32cf39cd170
Ngi Bujri <nbujri7@gmail.com>
2023-05-05 11:13:00 -0400

    create MongoDB collections

---
+++ slytherin-whatabook.js
@@ -0,0 +1,74 @@
+/**
+ * title: slytherin-whatabook.js
+ * author: ngi bujri, janis gonzalez, william egge
+ * date: may 5 2023
+ * description: script to build and populate MongoDB collection
+ */
+
+// drop collections
+db.books.drop();
+db.customers.drop();
+db.wishlistItems.drop();
+
+// create books collection
+db.createCollection("books", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["title", "genre", "author", "bookId"],
+      properties: {
+        title: {
+          bsonType: "string",
+        },
+        genre: {
+          bsonType: "string",
+        },
+        author: {
+          bsonType: "string",
+        },
+        bookId: {
+          bsonType: "string",
+        },
+      },
+    },
+  },
+});
+
+// create customers collection
+db.createCollection("customers", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["firstName", "lastName", "customerID"],
+      properties: {
+        firstName: {
+          bsonType: "string",
+        },
+        lastName: {
+          bsonType: "string",
+        },
+        customerId: {
+          bsonType: "string",
+        },
+      },
+    },
+  },
+});
+
+// create wishlistItems collection
+db.createCollection("wishlistItems", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["bookId", "customerId"],
+      properties: {
+        bookId: {
+          bsonType: "string",
+        },
+        customerId: {
+          bsonType: "string",
+        },
+      },
+    },
+  },
+});
