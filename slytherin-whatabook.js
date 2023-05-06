0448679b284816f93677ea4e01a64352de79d65f
Ngi Bujri <nbujri7@gmail.com>
2023-05-06 16:58:34 -0400

    rewrite collections and dummy data. add some queries

--- slytherin-whatabook.js
+++ slytherin-whatabook.js
@@ -8,7 +8,6 @@
 // drop collections
 db.books.drop();
 db.customers.drop();
-db.wishlistItems.drop();
 
 // COLLECTIONS
 
@@ -60,65 +59,132 @@ db.createCollection("customers", {
   },
 });
 
-// create wishlistItems collection
-db.createCollection("wishlistItems", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      required: ["bookId", "customerId"],
-      properties: {
-        bookId: {
-          bsonType: "string",
-        },
-        customerId: {
-          bsonType: "string",
-        },
-      },
-    },
-  },
-});
-
 // DUMMY DATA
 
 // books dummy data
-const booksDummyData = [];
-// create new documents and push to booksDummyData
-for (let i = 0; i < 5; i++) {
-  const newBook = {
-    title: `title${i + 1}`,
-    genre: `genre${i + 1}`,
-    author: `author${i + 1}`,
-    bookId: `b${i}`,
-  };
-  booksDummyData.push(newBook);
-}
-// insert documents from booksDummyData
+const book1 = {
+  title: "book1",
+  genre: "fantasy",
+  author: "author1",
+  bookId: "b1",
+};
+const book2 = {
+  title: "book2",
+  genre: "fantasy",
+  author: "author1",
+  bookId: "b2",
+};
+const book3 = {
+  title: "book3",
+  genre: "sci-fi",
+  author: "author2",
+  bookId: "b3",
+};
+const book4 = {
+  title: "book4",
+  genre: "mystery",
+  author: "author3",
+  bookId: "b4",
+};
+const book5 = {
+  title: "book5",
+  genre: "horror",
+  author: "author4",
+  bookId: "b5",
+};
+const booksDummyData = [book1, book2, book3, book4, book5];
+
+// insert booksDummyData into books collection
 db.books.insertMany(booksDummyData);
 
 // customers dummy data
-const customersDummyData = [];
-// create new documents and push to customersDummyData
-for (let i = 0; i < 5; i++) {
-  const newCustomer = {
-    firstName: `firstName${i + 1}`,
-    lastName: `lastName${i + 1}`,
-    customerId: `c${i}`,
-    wishlist: [],
-  };
-  customersDummyData.push(newCustomer);
-}
-// insert documents from customersDummyData
+const customer1 = {
+  firstName: "first1",
+  lastName: "last1",
+  customerId: "c1",
+  wishlist: [
+    {
+      bookId: "b1",
+    },
+    {
+      bookId: "b2",
+    },
+  ],
+};
+const customer2 = {
+  firstName: "first2",
+  lastName: "last2",
+  customerId: "c2",
+  wishlist: [
+    {
+      bookId: "b3",
+    },
+    {
+      bookId: "b4",
+    },
+  ],
+};
+const customer3 = {
+  firstName: "first3",
+  lastName: "last3",
+  customerId: "c3",
+  wishlist: [
+    {
+      bookId: "b5",
+    },
+    {
+      bookId: "b2",
+    },
+  ],
+};
+const customer4 = {
+  firstName: "first4",
+  lastName: "last4",
+  customerId: "c4",
+  wishlist: [
+    {
+      bookId: "b4",
+    },
+    {
+      bookId: "b5",
+    },
+  ],
+};
+const customer5 = {
+  firstName: "first5",
+  lastName: "last5",
+  customerId: "c5",
+  wishlist: [
+    {
+      bookId: "b3",
+    },
+    {
+      bookId: "b1",
+    },
+  ],
+};
+
+const customersDummyData = [
+  customer1,
+  customer2,
+  customer3,
+  customer4,
+  customer5,
+];
+
+// insert customersDummyData into customers collection
 db.customers.insertMany(customersDummyData);
 
-// wishlistItems dummy data
-const wishlistItemsDummyData = [];
-// create new documents and push to wishlistItemsDummyData
-for (let i = 0; i < 5; i++) {
-  const newWishlistItem = {
-    bookId: `b${i}`,
-    customerId: `c${i}`,
-  };
-  wishlistItemsDummyData.push(newWishlistItem);
-}
-// insert documents from wishlistItemsDummyData
-db.wishlistItems.insertMany(wishlistItemsDummyData);
+// QUERIES
+
+// display all books
+db.books.find();
+
+// display books by specified genre
+db.books.find({ genre: "fantasy" });
+
+// display books by specified author
+db.books.find({ author: "author2" });
+
+// display book by specified bookId
+db.books.find({ bookId: "b5" });
